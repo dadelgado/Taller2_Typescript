7395b993dbc92024d30c2839564819a1560586dc
dadelgado <da.delgado@uniandes.edu.co>
2020-03-30 23:18:43 -0500

    Cambio nombre

--- dataCourses.ts
+++ dataCourses.ts
@@ -4,7 +4,7 @@ export const dataCourses = [
     new Course("Planeacion de Transporte","Alvaro Rodriguez",  4),  
     new Course("Gestion de Trafico", "German Lleras", 4),
     new Course("Sistemas Transaccionales", "Anamaria Mojica", 3),
-    new Course("Desarrollo de Sofware", "Rubby Casallas", 3),
+    new Course("Desarrollo de Software", "Rubby Casallas", 3),
     new Course("Infraestructura de Comunicaciones", "Yezyd Donoso", 3),
     new Course("Modelado, Optimizacion y Simulacion", "German M", 3)
 ]
